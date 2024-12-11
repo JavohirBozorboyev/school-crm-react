6d4e1a93e7bbd38d813e5c9dad70ca50252d8c0b
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2024-12-11 14:08:28 +0500

    stundet table

--- src/module/StudentsPageSection/StudentPageTable.tsx
+++ src/module/StudentsPageSection/StudentPageTable.tsx
@@ -5,35 +5,36 @@ import {
   Badge,
   Button,
   Group,
-  Menu,
   Pagination,
-  rem,
   Table,
   Text,
 } from "@mantine/core";
 import {
   IconArrowRight,
-  IconBrandTelegram,
-  IconDots,
   IconPencil,
   IconPhone,
-  IconPlus,
   IconTrash,
 } from "@tabler/icons-react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useSWR from "swr";
+import AccessControl from "../../security/AccessControl";
 
 const StudentPageTable = () => {
-  const { data, error, isLoading } = useSWR(
-    "https://jsonplaceholder.typicode.com/users"
-  );
+  const { data, error, isLoading } = useSWR("/api/students");
   if (error) return <div>ошибка загрузки</div>;
   if (isLoading) return <div>загрузка...</div>;
 
+
   return (
     <>
-      <Table.ScrollContainer minWidth={1400}>
-        <Table verticalSpacing={"sm"} striped withTableBorder mt={"md"}>
+      <Table.ScrollContainer minWidth={1600}>
+        <Table
+          verticalSpacing={"sm"}
+          withTableBorder
+          mt={"md"}
+          withColumnBorders
+          highlightOnHover
+        >
           <Table.Thead>
             <Table.Tr>
               <Table.Th ta={"center"}>N</Table.Th>
@@ -41,19 +42,21 @@ const StudentPageTable = () => {
               <Table.Th w={100}>Sinf</Table.Th>
               <Table.Th>Passport</Table.Th>
               <Table.Th>Kantaktlar</Table.Th>
-              <Table.Th>Manzil</Table.Th>
+              <Table.Th w={350}>Manzil</Table.Th>
               <Table.Th w={250}>Ota-Ona ismi</Table.Th>
               <Table.Th align="center" miw={150}>
                 Ota-Ona Kantaktlari
               </Table.Th>
 
-              <Table.Th ta={"center"}>Tahrirlash</Table.Th>
-              <Table.Th ta={"center"} w={150}></Table.Th>
+              <Table.Th ta={"center"} w={120}>
+                Tahrirlash
+              </Table.Th>
+              <Table.Th ta={"center"} w={120}></Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
-            {data?.map((element: any, i: number) => (
-              <Table.Tr key={element.name}>
+            {data?.map((item: any, i: number) => (
+              <Table.Tr key={i}>
                 <Table.Td w={50} ta={"center"}>
                   {i + 1}
                 </Table.Td>
@@ -61,109 +64,76 @@ const StudentPageTable = () => {
                   <Group gap={"xs"}>
                     {" "}
                     <Avatar color="blue" size={"sm"} radius={"sm"} />
-                    <Text size="sm">{element.name}</Text>
+                    <Text size="sm">{item?.fullname}</Text>
                   </Group>
                 </Table.Td>
                 <Table.Td>
                   <Badge size="md" variant="light" color="indigo">
-                    7-Green
+                    {item?.group?.title}
                   </Badge>
                 </Table.Td>
-                <Table.Td>AA1234567</Table.Td>
+                <Table.Td>{item?.passport}</Table.Td>
                 <Table.Td>
-                  <ActionIcon variant="light" radius={"xl"}>
-                    <IconPhone size={16} />
-                  </ActionIcon>
-                  <ActionIcon variant="light" radius={"xl"} disabled ml={"xs"}>
-                    <IconBrandTelegram size={16} />
-                  </ActionIcon>
+                  <NavLink to={`tel:${item?.phone}`}>
+                    <ActionIcon variant="light" radius={"xl"}>
+                      <IconPhone size={16} />
+                    </ActionIcon>
+                  </NavLink>
                 </Table.Td>
                 <Table.Td>
-                  <Text size="sm">{element.address.city}</Text>
+                  <Text size="sm">{item?.address}</Text>
                 </Table.Td>
                 <Table.Td>
                   <Group gap={"xs"}>
                     {" "}
                     <Avatar color="cyan" size={"sm"} radius={"sm"} />
-                    <Text size="sm">{element.name}</Text>
+                    <Text size="sm">{item?.father}</Text>
                   </Group>
                 </Table.Td>
                 <Table.Td>
-                  <ActionIcon variant="light" radius={"xl"}>
-                    <IconPhone size={16} />
-                  </ActionIcon>
-                  <ActionIcon variant="light" radius={"xl"} disabled ml={"xs"}>
-                    <IconBrandTelegram size={16} />
-                  </ActionIcon>
-                  <Menu shadow="md" width={200} withArrow>
-                    <Menu.Target>
-                      <ActionIcon variant="light" radius={"xl"} ml={"xs"}>
-                        <IconDots size={16} />
-                      </ActionIcon>
-                    </Menu.Target>
-
-                    <Menu.Dropdown>
-                      <Menu.Label>Contactlar</Menu.Label>
-                      <Menu.Item
-                        leftSection={
-                          <IconPhone
-                            style={{ width: rem(14), height: rem(14) }}
-                          />
-                        }
-                      >
-                        Contact 1
-                      </Menu.Item>
-                      <Menu.Item
-                        leftSection={
-                          <IconPhone
-                            style={{ width: rem(14), height: rem(14) }}
-                          />
-                        }
-                      >
-                        Contact 2
-                      </Menu.Item>
-                      <Menu.Divider />
-                      <Menu.Label>Contact Taxrirlash</Menu.Label>
-                      <Menu.Item
-                        leftSection={
-                          <IconPlus
-                            style={{ width: rem(14), height: rem(14) }}
-                          />
-                        }
-                        color="cyan"
-                      >
-                        Yangi Contact
-                      </Menu.Item>
-                      <Menu.Item
-                        leftSection={
-                          <IconTrash
-                            style={{ width: rem(14), height: rem(14) }}
-                          />
-                        }
-                        color="red"
-                      >
-                        Contact O'chirish
-                      </Menu.Item>
-                    </Menu.Dropdown>
-                  </Menu>
+                  <NavLink to={`tel:${item?.fatherPhone}`}>
+                    <ActionIcon variant="light" radius={"xl"}>
+                      <IconPhone size={16} />
+                    </ActionIcon>
+                  </NavLink>
+                  <NavLink to={`tel:${item?.motherPhone}`}>
+                    <ActionIcon
+                      color="indigo"
+                      variant="light"
+                      radius={"xl"}
+                      ml={"xs"}
+                    >
+                      <IconPhone size={16} />
+                    </ActionIcon>
+                  </NavLink>
                 </Table.Td>
 
                 <Table.Td ta={"center"}>
-                  <ActionIcon mr={"xs"} variant="light" color="red">
-                    <IconTrash size={16} />
-                  </ActionIcon>
-                  <ActionIcon variant="light" color="blue">
-                    <IconPencil size={16} />
-                  </ActionIcon>
+                  <AccessControl
+                    requiredPermissions={["delete", "read"]}
+                    requiredPrivileges={["manage_users", "view_reports"]}
+                  >
+                    <ActionIcon mr={"xs"} variant="light" color="red">
+                      <IconTrash size={16} />
+                    </ActionIcon>
+                  </AccessControl>
+                  <AccessControl
+                    requiredPermissions={["update", "read"]}
+                    requiredPrivileges={["manage_users", "view_reports"]}
+                  >
+                    <ActionIcon variant="light" color="blue">
+                      <IconPencil size={16} />
+                    </ActionIcon>
+                  </AccessControl>
                 </Table.Td>
                 <Table.Td ta={"center"}>
-                  <Link to={`/students/${element.id}`}>
+                  <Link to={`/students/${item.id}`}>
                     <Button
                       variant="subtle"
                       size="xs"
                       rightSection={<IconArrowRight size={16} />}
                     >
-                      Malumotlar
+                      Ko'rish
                     </Button>{" "}
                   </Link>
                 </Table.Td>
